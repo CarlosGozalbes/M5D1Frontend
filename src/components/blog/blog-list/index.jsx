a36d8aa0f9041546178494675fef7f75e7cbad7c
Carlos Gozalbes <carlosfgmendoza@gmail.com>
2022-02-01 16:03:26 +0100

    fdas

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -3,12 +3,32 @@ import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
 export default class BlogList extends Component {
+  state = { blogPosts: [] };
+  fetchBlogs = async () => {
+    try {
+      const apiUrl = process.env.REACT_APP_BE_URL;
+      let response = await fetch(`${apiUrl}/blogsPosts`, {
+        method: "GET",
+      });
+      if (response.ok) {
+        let blogs = await response.json();
+        console.log("Blogsposts", blogPosts);
+        this.setState({ blogPosts: blogPosts });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  componentDidMount = () => {
+    this.fetchBlogs();
+  };
+
   render() {
     return (
       <Row>
-        {posts.map((post) => (
-          <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
+        {blogPosts.map((post) => (
+          <Col  md={4} style={{ marginBottom: 50 }}>
+            <BlogItem key={blogPost._id} {...blogPost} />
           </Col>
         ))}
       </Row>
