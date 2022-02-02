c29f53252642f799aeae9dd512abc00274fd76ee
Carlos Gozalbes <carlosfgmendoza@gmail.com>
2022-02-02 16:10:00 +0100

    download button

--- src/components/blog/blog-item/index.jsx
+++ src/components/blog/blog-item/index.jsx
@@ -4,20 +4,40 @@ import BlogAuthor from "../blog-author";
 import { Link } from "react-router-dom";
 import "./styles.css";
 export default class BlogItem extends Component {
+  
+  
+  
+  
   render() {
     const { title, cover, author, _id } = this.props;
+    
+    
+    const downloadPDF = async () => {
+      try {
+        const apiUrl = process.env.REACT_APP_BE_URL
+        let response = await fetch(`${apiUrl}/blogPosts/${_id}/downloadPDF`, {
+          method: "GET",
+        })
+        
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    
     return (
-      <Link to={`/blogPost/${_id}`} className="blog-link">
-        <Card className="blog-card">
-          <Card.Img variant="top" src={cover} className="blog-cover" />
-          <Card.Body>
+      <Card className="blog-card">
+        <Card.Img variant="top" src={cover} className="blog-cover" />
+        <Card.Body>
+          <Link to={`/blogPost/${_id}`} className="blog-link">
             <Card.Title>{title}</Card.Title>
-          </Card.Body>
-          <Card.Footer>
-            <BlogAuthor {...author} />
-          </Card.Footer>
-        </Card>
-      </Link>
+          </Link>
+        </Card.Body>
+        <Card.Footer>
+          <BlogAuthor {...author} />
+          <button onClick={downloadPDF}>Download PDF</button>
+        </Card.Footer>
+      </Card>
     );
   }
 }
